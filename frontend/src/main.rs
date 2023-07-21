d0d344f24a6f2a6b4431473a2c6785e71edae6ed
Heaven Chen <heavenchen@qnap.com>
2023-07-22 06:50:50 +0800

    Remove Lang import

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,18 +1,15 @@
 use frontend::layout::footer::Footer;
 use frontend::layout::navbar::Navbar;
-use frontend::modules::lang::Lang;
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let lang = use_state(|| Lang::CHN);
-
     html! {
         <div>
             <Navbar />
-            <main class="h-96 w-10/12 bg-sky-500 mt-10 flex mx-auto">
+            <main class="h-96 w-11/12 bg-sky-500 mt-10 flex mx-auto">
             </main>
-            <Footer lang={*lang.clone()}/>
+            <Footer/>
         </div>
     }
 }
