5d42d94dca6e099ad16491fc27d5dcac3dae59d4
Heaven Chen <heavenchen@qnap.com>
2023-09-02 16:12:49 +0800

    Add patterns for backend api call

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -5,3 +5,25 @@ pub struct ResponseHeadline {
     pub status: String,
     pub headlines: Vec<(String, String)>,
 }
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct User {
+    pub id: uuid::Uuid,
+    pub email: String,
+    pub name: Option<String>,
+    pub company: Option<String>,
+    pub phone: Option<String>,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Email {
+    pub id: uuid::Uuid,
+    pub user_id: uuid::Uuid,
+    pub title: String,
+    pub service_type: String,
+    pub body: String,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
